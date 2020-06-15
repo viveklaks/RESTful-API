3a40a830f4d2fbcaf5f85c9e7ffb088a5535e872
Vivek H <viveklaks@gmail.com>
2020-06-15 17:02:34 +0530

    lesson-5-MongoDB-and-Mongoose-|-Creating-a-REST-API-with-Node.js
    create a MongoDB atlas project and set the configuration which includes IP-address, user id and password for the particular Database and install mongoose and create models for product ad making codes for the CRUD using different HTTP requests

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+mongoose.connect("mongodb+srv://vivekHDB:DBlockin@2688@cluster0-4mf3g.mongodb.net/test?retryWrites=true&w=majority"
+, { useUnifiedTopology: true , useNewUrlParser: true }
+);
 app.use(morgan('dev'));
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req,res,next)=>{
+    res.header('Access-control-Allow-Origin','*');
+    res.header('Access-control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization');
+    if(req.method === "OPTIONS"){
+        res.header('Access-control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+
+        
+    }
+    next();
+});
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
 
